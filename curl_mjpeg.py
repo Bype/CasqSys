c53cc6363b85bebf356cf08376a166cab5d1f0f5
David Olivari <david.olivari@gmail.com>
2013-04-10 07:25:30 +0200

    Objectified Add stream switch Add timeout

--- curl_mjpeg.py
+++ curl_mjpeg.py
@@ -1,7 +1,4 @@
 #! /usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-# vi:ts=4:et
-# $Id: basicfirst.py,v 1.5 2005/02/11 11:09:11 mfx Exp $
 
 import sys
 import pycurl
@@ -9,63 +6,93 @@ import subprocess
 import os, tempfile
 import datetime
 import RPIO
+import redis
+import random
 from time import sleep
 from thread import allocate_lock
 
-RPIO.setmode(RPIO.BCM)
-RPIO.setup(17, RPIO.IN, pull_up_down=RPIO.PUD_UP)
-
-
-tmpdir = tempfile.mkdtemp()
-filename = os.path.join(tmpdir, 'fifo.mjpeg')
-print (filename)
-try:
-    os.mkfifo(filename)
-except e:
-    print ("Failed to create FIFO: %s" % e)
-
-cmd = ['omxplayer', '-r', filename]
-p = subprocess.Popen(cmd, stdin=subprocess.PIPE,bufsize=0, stderr=subprocess.STDOUT)
-f = open(filename,'w')
-st = False
-lock = allocate_lock()
-
-def body_callback(buf):
-    f.write(buf)
-    lock.acquire()
-    mySt = st
-    lock.release()
-    if(mySt):
-        return -1
-   
-
-def gpio_callback(gpio_id, val):
-    print("gpio %s: %s" % (gpio_id, val))
-    lock.acquire()
-    global st 
-    st = True
-    lock.release()      
-
-RPIO.add_interrupt_callback(17, gpio_callback, edge='rising', debounce_timeout_ms=100)
-RPIO.wait_for_interrupts(threaded=True)
-
-
-while(True):
-    print "Starting..."
-    lock.acquire()
-    st=False
-    lock.release()
-    c = pycurl.Curl()
-    c.setopt(c.URL, 'http://192.168.2.195/?action=stream')
-    c.setopt(c.BUFFERSIZE,32768)
-    c.setopt(c.WRITEFUNCTION, body_callback)
-    try:
-        c.perform()
-    except:
-        c.close()
-        print "Changing Stream"
-        sleep(5)
-        print "Again ?"
+class Player(object):
+    def __init__(self, timeo):
+        self.lock = allocate_lock()
+        self.isChangedAsked = False
+        self.isTimedOut = False 
+        self.lastChange = datetime.datetime.now();
+        self.localtimeout = timeo
+        self.currentHost = 'localhost'
         
+    def setupRPIO(self):
+        RPIO.setmode(RPIO.BCM)
+        RPIO.setup(17, RPIO.IN, pull_up_down=RPIO.PUD_UP)
+        RPIO.add_interrupt_callback(17, self.gpio_callback, edge='rising', debounce_timeout_ms=5000)
+        RPIO.wait_for_interrupts(threaded=True)
         
+    def gpio_callback(self, gpio_id, val):
+        self.lock.acquire()
+        self.isChangedAsked = True
+        self.isTimedOut = False
+        self.lock.release()      
+        
+    def startPlayer(self):
+        tmpdir = tempfile.mkdtemp()
+        filename = os.path.join(tmpdir, 'fifo.mjpeg')
+        try:
+            os.mkfifo(filename)
+        except e:
+            print ("Failed to create FIFO: %s" % e)
+        cmd = ['omxplayer', '-r', filename]
+        p = subprocess.Popen(cmd, stdin=subprocess.PIPE, bufsize=0, stderr=subprocess.STDOUT)
+        self.fifo = open(filename, 'w')
+        
+    def body_callback(self, buf):
+        self.fifo.write(buf)
+        self.lock.acquire()
+        if(self.localtimeout < (datetime.datetime.now() - self.lastChange).seconds):
+            self.isTimedOut = True
+            if (self.currentHost != 'localhost'):
+                aChangedAsked = True
+        else:
+            aChangedAsked = self.isChangedAsked
+        self.lock.release()    
+        if(aChangedAsked):
+            return -1 
+        
+
+    def connectTo(self, host):
+        self.lock.acquire()
+        self.isChangedAsked = False
+        if(self.isTimedOut):
+            host = 'localhost'
+        self.isTimedOut = False
+        self.lastChange = datetime.datetime.now();
+        self.currentHost = host
+        self.lock.release()
+        print 'connecting to ' + host
+        self.connection = pycurl.Curl()
+        self.connection.setopt(self.connection.URL, 'http://%s/?action=stream' % (host))
+        self.connection.setopt(self.connection.BUFFERSIZE, 32768)
+        self.connection.setopt(self.connection.WRITEFUNCTION, self.body_callback)
+        
+    def setupRedis(self):
+        self.red = redis.StrictRedis(host='localhost', port=6379, db=0)
+        
+    def switchStream(self):
+        try:
+            self.connectTo(random.sample(red.smembers('ip'), 1))
+        except:
+            self.connectTo('localhost')
+
+    def main(self):
+        self.setupRPIO()
+        self.setupRedis()
+        self.startPlayer()
+        self.connectTo('localhost')
+        while(True):
+            try:
+                self.connection.perform()
+            except:
+                self.connection.close()
+                self.switchStream()
+            
+if __name__ == "__main__":
+    Player(60).main()
 
